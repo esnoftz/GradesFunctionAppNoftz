c0b7a580916fbc686b8cbcb4faca2597a63c8d4e
Evangeline Noftz <esnoftz@gmail.com>
2024-09-18 07:49:34 -0500

    Initial Commit

---
+++ GradesFunctionAppNoftzTests/GradesFunctionAppNoftzTests.swift
@@ -0,0 +1,17 @@
+//
+//  GradesFunctionAppNoftzTests.swift
+//  GradesFunctionAppNoftzTests
+//
+//  Created by EVANGELINE NOFTZ on 9/18/24.
+//
+
+import Testing
+@testable import GradesFunctionAppNoftz
+
+struct GradesFunctionAppNoftzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
