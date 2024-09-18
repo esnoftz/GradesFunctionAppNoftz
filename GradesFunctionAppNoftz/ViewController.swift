11c778cc5484f743ca889a751cfc3ac9e50760aa
Evangeline Noftz <esnoftz@gmail.com>
2024-09-18 08:09:46 -0500

    Initial Commit.

--- GradesFunctionAppNoftz/ViewController.swift
+++ GradesFunctionAppNoftz/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBOutlet weak var earnedTextField: UITextField!
+    
+    @IBOutlet weak var totalTextField: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print(passOrFail(earned: 20.3, total: 70.6))
+        
+        
     }
 
+    
+    func passOrFail(earned: Double, total: Double) -> String {
+        
+        var percent = (earned/total)*100.0
+        if (percent >= 60) {
+            return "You passed! :)"
+        } else {
+            return "You failed! :("
+        }
+        
+    }
+    
+    func nothing() -> String {
+        
+        return "Error! This is nothing!"
+        
+    }
+    
+    
+    
+    @IBAction func submitAction(_ sender: UIButton) {
+        
+        var earnedPoints = Double(earnedTextField.text!)!
+        var totalPoints = Double(totalTextField.text!)!
+        
+        var answer = passOrFail(earned: earnedPoints, total: totalPoints)
+        
+        errorLabel.text = answer
+        
+    }
+    
+    
+    
+    
+    
+    
 
 }
 
