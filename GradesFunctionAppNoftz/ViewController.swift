bd780cd742fa0e9034d87569c7eb724bbc1148f9
Evangeline Noftz <esnoftz@gmail.com>
2024-09-19 08:06:55 -0500

    Initial commit.

--- GradesFunctionAppNoftz/ViewController.swift
+++ GradesFunctionAppNoftz/ViewController.swift
@@ -17,6 +17,19 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var totalTextField: UITextField!
     
+    @IBOutlet weak var percentTextField: UITextField!
+    
+    @IBOutlet weak var submitButton2: UIButton!
+    
+    @IBOutlet weak var errorLabel2: UILabel!
+    
+    @IBOutlet weak var earnedTextField2: UITextField!
+    
+    @IBOutlet weak var totalTextField2: UITextField!
+    
+    @IBOutlet weak var submitButton3: UIButton!
+    
+    @IBOutlet weak var errorLabel3: UILabel!
     
     
     
@@ -40,27 +53,70 @@ class ViewController: UIViewController {
         
     }
     
-    func nothing() -> String {
-        
-        return "Error! This is nothing!"
-        
+    func errorFunction() -> String {
+        return "ERROR!"
     }
     
+    func percentGrade(percent: Double) -> String {
+        
+        if percent >= 90.0 {
+            return "Your Grade: A"
+        } else if percent >= 80.0 {
+            return "Your Grade: B"
+        } else if percent >= 70.0 {
+            return "Your Grade: C"
+        } else if percent >= 60.0 {
+            return "Your Grade: D"
+        } else {
+            return "Your Grade: F"
+        }
+    }
     
+    func percentAndLetterGrade(earned: Double, total: Double) -> String {
+        
+        
+        
+    }
     
     @IBAction func submitAction(_ sender: UIButton) {
         
-        var earnedPoints = Double(earnedTextField.text!)!
-        var totalPoints = Double(totalTextField.text!)!
-        
-        var answer = passOrFail(earned: earnedPoints, total: totalPoints)
-        
-        errorLabel.text = answer
+        if let earnedPoints = Double(earnedTextField.text!) {
+            if let totalPoints = Double(totalTextField.text!) {
+                var answer = passOrFail(earned: earnedPoints, total: totalPoints)
+                errorLabel.text = answer
+            } else {
+                errorLabel.text = errorFunction()
+            }
+        } else {
+            errorLabel.text = errorFunction()
+        }
         
     }
     
     
+    @IBAction func submitAction2(_ sender: UIButton) {
+        
+        if let check = Double(percentTextField.text!) {
+            errorLabel2.text = percentGrade(percent: check)
+        } else {
+            errorLabel2.text = errorFunction()
+        }
+        
+    }
     
+    @IBAction func submitAction3(_ sender: UIButton) {
+        
+        if let earnedPoints = Double(earnedTextField2.text!) {
+            if let totalPoints = Double(totalTextField2.text!) {
+                percentAndLetterGrade(earned: earnedPoints, total: totalPoints)
+            } else {
+                errorLabel3.text = errorFunction()
+            }
+        } else {
+            errorLabel3.text = errorFunction()
+        }
+        
+    }
     
     
     
